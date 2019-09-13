6f1156c31e5d159781a6fa23deed1106af0fc9a0
Rodion <rodion77777@gmail.com>
2019-09-13 16:14:39 +0300

    commitCucumer

--- src/main/java/com/aplana/SBCucumber/Trash.java
+++ src/main/java/com/aplana/SBCucumber/Trash.java
@@ -2,30 +2,11 @@ package com.aplana.SBCucumber;
 
 import org.openqa.selenium.WebDriver;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Trash {
-    private static Map<String,String> map = new HashMap<>();
-
     public static WebDriver driver;
-
-    public static String get(String key){
-        return map.get(key);
-    }
     public static WebDriver getDriver(){
         return driver;
     }
-    public static void put(String key, String value){
-        map.put(key, value);
-    }
-    public static int summa(){
-        int num = 0;
-        for (String value : map.values()) {
 
-            num += Integer.parseInt(value);
-        }
-        return num;
     }
 
-}
